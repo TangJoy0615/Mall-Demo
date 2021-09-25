8e08d93c67a75bc5a885e8abd84e08eb37129eaa
Joy Tang <1298355807@qq.com>
2021-09-25 22:54:17 +0800

    first commmit

---
+++ src/main/java/priv/jesse/mall/web/admin/AdminProductController.java
@@ -0,0 +1,153 @@
+package priv.jesse.mall.web.admin;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import priv.jesse.mall.entity.Classification;
+import priv.jesse.mall.entity.Product;
+import priv.jesse.mall.entity.pojo.ResultBean;
+import priv.jesse.mall.service.ClassificationService;
+import priv.jesse.mall.service.ProductService;
+import priv.jesse.mall.utils.FileUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/mall/admin/product")
+public class AdminProductController {
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ClassificationService classificationService;
+
+    @RequestMapping("/toList.html")
+    public String toList() {
+        return "admin/product/list";
+    }
+
+    @RequestMapping("/toAdd.html")
+    public String toAdd() {
+        return "admin/product/add";
+    }
+
+    @RequestMapping("/toEdit.html")
+    public String toEdit(int id, Map<String, Object> map) {
+        Product product = productService.findById(id);
+        Classification classification = classificationService.findById(product.getCsid());
+        product.setCategorySec(classification);
+        map.put("product", product);
+        return "admin/product/edit";
+    }
+
+    @ResponseBody
+    @RequestMapping("/list.do")
+    public ResultBean<List<Product>> listProduct(int pageindex,
+                                                 @RequestParam(value = "pageSize", defaultValue = "15") int pageSize) {
+        Pageable pageable = PageRequest.of(pageindex, pageSize);
+        List<Product> list = productService.findAll(pageable).getContent();
+        return new ResultBean<>(list);
+    }
+
+    @ResponseBody
+    @RequestMapping("/getTotal")
+    public ResultBean<Integer> getTotal() {
+        Pageable pageable = PageRequest.of(1, 15);
+        int total = (int) productService.findAll(pageable).getTotalElements();
+        return new ResultBean<>(total);
+    }
+
+    @RequestMapping("/del.do")
+    @ResponseBody
+    public ResultBean<Boolean> del(int id) {
+        productService.delById(id);
+        return new ResultBean<>(true);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/add.do")
+    public void add(MultipartFile image,
+                    String title,
+                    Double marketPrice,
+                    Double shopPrice,
+                    int isHot,
+                    String desc,
+                    int csid,
+                    HttpServletRequest request,
+                    HttpServletResponse response) throws Exception {
+        Product product = new Product();
+        product.setTitle(title);
+        product.setMarketPrice(marketPrice);
+        product.setShopPrice(shopPrice);
+        product.setDesc(desc);
+        product.setIsHot(isHot);
+        product.setCsid(csid);
+        product.setPdate(new Date());
+        String imgUrl = FileUtil.saveFile(image);
+        product.setImage(imgUrl);
+        int id = productService.create(product);
+        if (id <= 0) {
+            request.setAttribute("message", "添加失败！");
+            request.getRequestDispatcher("toAdd.html").forward(request, response);
+        } else {
+            request.getRequestDispatcher("toEdit.html?id=" + id).forward(request, response);
+        }
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST, value = "/update.do")
+    public void update(int id,
+                       String title,
+                       Double marketPrice,
+                       Double shopPrice,
+                       String desc,
+                       int csid,
+                       int isHot,
+                       MultipartFile image,
+                       HttpServletResponse response) throws Exception {
+        Product product = productService.findById(id);
+        product.setTitle(title);
+        product.setMarketPrice(marketPrice);
+        product.setShopPrice(shopPrice);
+        product.setDesc(desc);
+        product.setIsHot(isHot);
+        product.setCsid(csid);
+        product.setPdate(new Date());
+        String imgUrl = FileUtil.saveFile(image);
+        if (StringUtils.isNotBlank(imgUrl)) {
+            product.setImage(imgUrl);
+        }
+        try {
+            productService.update(product);
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+        response.sendRedirect("toList.html");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/img/{filename:.+}")
+    public void getImage(@PathVariable(name = "filename") String filename,
+                         HttpServletResponse res) throws IOException {
+        File file = new File("file/" + filename);
+        if (file.exists()) {
+            res.setHeader("content-type", "application/octet-stream");
+            res.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, String.valueOf(StandardCharsets.UTF_8)));
+            res.setContentLengthLong(file.length());
+            Files.copy(Paths.get(file.toURI()), res.getOutputStream());
+        }
+    }
+
+
+}
